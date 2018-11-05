e90796d5ea19322cc86e4ac758f0a7a3bfa1ede3
Jakub Pavlik <pavlk.jakub@gmail.com>
2018-11-05 10:33:51 +0100

    remove annotation

--- main.go
+++ main.go
@@ -59,8 +59,7 @@ func mutateServices(ar v1beta1.AdmissionReview, o *options) *v1beta1.AdmissionRe
 
 	addPodHostNetworking := fmt.Sprintln(`[
 		{"op":"add","path":"/spec/hostNetwork","value": true},
-		{"op":"add","path":"/spec/dnsPolicy","value": "ClusterFirstWithHostNet"},
-		{"op":"add","path":"/metadata/annotations","value":{"ves.io/mutating-webhook":"hostnetwork-dnspolicy"}}
+		{"op":"add","path":"/spec/dnsPolicy","value": "ClusterFirstWithHostNet"}
    ]`)
 
 	glog.V(2).Infof("patching hostNetworking to pod: %v", pod.ObjectMeta.Name)
